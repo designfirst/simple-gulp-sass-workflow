2085b05c3632b4644d98faf40b96839a2fa1a87c
designfirst <designfirst@gmail.com>
2015-10-14 17:03:47 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var input = './app/sass/**/*.scss';
+var output = './public/css/';
+
+
+gulp.task('styles', function() {
+    return gulp
+        // Find all `.scss` files from the `stylesheets/` folder
+        .src(input)
+        // Run Sass on those files
+        .pipe(sass())
+        // Write the resulting CSS in the output folder
+        .pipe(gulp.dest(output));
+});
+
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch(input,['styles']);
+});
+
